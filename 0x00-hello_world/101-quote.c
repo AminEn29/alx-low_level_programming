84042c6e3920a31d7758a23ec60aa9cfbc042ba4
Mohamed amine <aminework229@gmail.com>
2023-08-13 06:45:45 -0700

    hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry piont
+ * return 1 in success
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
