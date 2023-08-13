addb85bede0c74f338fe02c841735bc0b7dfedfc
Mohamed amine <aminework229@gmail.com>
2023-08-13 07:16:03 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry piont
- * return 1 in success
+ * main - main function
+ *
+ * Return: 1
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
