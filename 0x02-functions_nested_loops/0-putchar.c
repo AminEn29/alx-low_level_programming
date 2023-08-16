fefc50ed7b4287b9b619a3f1fb146ad50b3ea806
mohamed amine <aminework229@gmail.com>
2023-08-16 22:10:24 +0000

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
  * main - Entry point
@@ -14,8 +14,8 @@ int main(void)
 	int ch;
 
 	for (ch = 0; ch < 8; ++ch)
-		putchar(str[ch]);
-	putchar('\n');
+		_putchar(str[ch]);
+	_putchar('\n');
 
 	return (0);
 }
