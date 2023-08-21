69614c578904cf7f8859e4aa039393443c008681
mohamed amine <aminework229@gmail.com>
2023-08-21 14:30:15 +0000

    hello

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - check the code
+*
+*Return : alwasys 0
+*/
+int main(void)
+{
+    int n;
+    n = 402;
+    printf("n=%d\n", n);
+    rest_to_98(&n);
+    return(0);
+}
