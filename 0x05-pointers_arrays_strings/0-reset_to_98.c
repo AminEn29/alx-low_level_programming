c70e1059b13eeae26d3c3ac4776221ef5e2ee330
mohamed amine <aminework229@gmail.com>
2023-08-21 16:44:35 +0000

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
+*   @n : int to check
 */
 
-int main (void){
-	reset_to_98();
-	return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,13 +2,5 @@
 #define MAIN_H
 #include <stdio.h>
 
-void reset_to_98()
-{
-    int n = 402;
-    printf("n=%d\n", n);
-    int *ptr = NULL;
-     ptr = &n;
-    *ptr = 98;
-    printf("n=%d\n", *ptr);
-}
+void reset_to_98(int *n);
 #endif
