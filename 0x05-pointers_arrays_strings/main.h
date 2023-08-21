5b249e2b5bf021b2f52c7f178201a80560a30001
mohamed amine <aminework229@gmail.com>
2023-08-21 18:33:10 +0000

    hello

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void put(char *str);
+void _puts(char *str);
 void print_rev(char *s);
 void re_string(char *s);
 void puts2(char *str);
