0676fcbbfafe9557da203ee4a7b47b52c9e550dd
mohamed amine <aminework229@gmail.com>
2023-08-25 22:38:22 +0000

    hi

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* reverse_array - function that rever an array according to n
+* @a: is the arry
+* @n: is the element of arry
+*/
+void reverse_array(int *a, int n)
+{
+	int *p1 = a;
+	int *p2 = a + n - 1;
+	int temp;
+
+	while (p1 < p2)
+	{
+		temp = *p1;
+		*p1 = *p2;
+		while (*p2 = temp)
+		{
+			p1++;
+			p2--;
+		}
+	}
+}
