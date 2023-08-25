5a61cf70b09bfa2992964ae97e5ce0351009ab2b
mohamed amine <aminework229@gmail.com>
2023-08-25 18:56:56 +0000

    ?

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0', i++)
+	;
+	for (j = 0; src[n] != '\0', n++)
+	j = n
+	dest[i + j] = src[j];
+	return (dest);
+
+}
