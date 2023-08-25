d8756d42963920211651e3d574e3823538312e88
mohamed amine <aminework229@gmail.com>
2023-08-25 20:57:46 +0000

    hello

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,13 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-		int i, j;
+    int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	;
-	/*here when j > n the loopt will end and will printf until src[n]*/
-	for (j = 0; j < n && src[j] != '\0'; j++)
-	dest[i + j] = src[j];
-	if (j < n)
-		dest[i + j] = '\0';
-	return (dest);
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for (; i < n; i++)
+        dest[i] = '\0';
+
+    return dest;
 }
+
