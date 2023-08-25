064d1bcbb13e020dc9b659325e5af697a5172dc4
mohamed amine <aminework229@gmail.com>
2023-08-25 21:14:01 +0000

    hello

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strcmp - function that compaire two string
+* @s1: is the first string
+* @s2: is the second string**/
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2) {
+		printf("15\n");
+	}
+	else 
+	{
+		if (s1 < s2)
+		printf("-15\n");
+		else
+		printf("0\n");
+	}
+}
