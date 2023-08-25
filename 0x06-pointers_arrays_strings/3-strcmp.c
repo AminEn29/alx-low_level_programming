e3fe8d451edd194026635df7816925c556db8d11
mohamed amine <aminework229@gmail.com>
2023-08-25 22:07:57 +0000

    hello

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	;
-	for (j = 0; s2[j] != '\0'; j++)
-	;
+		while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 	if (*s1 > *s2)
 		return (15);
 	else if (*s1 < *s2)
