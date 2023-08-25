12f026ddbc00b1e0f18f8d11c0b9c0345f2dd08e
mohamed amine <aminework229@gmail.com>
2023-08-25 19:02:04 +0000

    hello

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+* _strncat - function that concatenates according to n
+* @dest: isthe distination source
+* @src: is the txt that will addd to dest
+* @n: how much charatare will type
+* Return: des
 */
 
 char *_strncat(char *dest, char *src, int n)
@@ -8,8 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0', i++)
 	;
-	for (j = 0; src[n] != '\0', n++)
-	j = n
+	for (j = n; src[n] != '\0', n++)
 	dest[i + j] = src[j];
 	return (dest);
 
