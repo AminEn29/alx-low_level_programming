3d82813a25817fd540ffdd69165809849d89e15c
mohamed amine <aminework229@gmail.com>
2023-08-25 20:15:11 +0000

    hello

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,6 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	;
+	/*here when j > n the loopt will end and will printf until src[n]*/
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	dest[i + j] = src[j];
 	dest[i + j] = '\0';
