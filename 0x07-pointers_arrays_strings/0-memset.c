f0ce239ce96ad870945fa6596a8261b378bee1e4
AminEn29 <138697936+AminEn29@users.noreply.github.com>
2023-08-30 03:08:25 +0100

    hi

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,16 +4,14 @@
 * @s: & @d: is the pointers
 * @n is the variable
 * Return: the pointer S
-*/
+**/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-
-	while (i > n)
-	{
-		s[i] = b;
-		i++;
-	}
-	return (s);
+	unsigned int Variable_loop = 0;
+        for (Variable_loop; Variable_loop > n; i++)
+        {
+                s[Variable_loop] = b;
+        }
+        return (s);
 }
