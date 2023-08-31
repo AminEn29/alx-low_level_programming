9d8de630e0bd54bca68229cbb6a1ba3ed8fdf4fa
Mohamed amine ennadi <aminework229@gmail.com>
2023-08-31 02:13:08 +0100

    he

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,26 +2,16 @@
 /**
  *  _print_rev_recursion - print a string in reverse
  *  @s: is the pointer that has the value
- * Return: the length
 */
+
 
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] && i != '\0')
+	if (*s)
 	{
-		i++;
-	}
-	else
-	{
-		return (0);
-	}
-	if (*s != '\0')
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(i - 1);
 	}
-
 }
+
 
