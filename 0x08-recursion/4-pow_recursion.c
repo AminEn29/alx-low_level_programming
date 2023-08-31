f73fcf0d952098b55c544be30b5c50d542170078
Mohamed amine ennadi <aminework229@gmail.com>
2023-08-31 03:14:57 +0100

    hee

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - function raised x power of y
+ * @x: is the base
+ * @y: is the exp
+ * Return: the result
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y != 0)
+	{
+		y--;
+		return (x * x);
+	}
+}
