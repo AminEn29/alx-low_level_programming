c94ed58b82f4e816bb0cab0689794bad639881f2
Mohamed amine ennadi <aminework229@gmail.com>
2023-08-31 02:19:56 +0100

    heelo

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _strlen_recursion - function done the length of a sting
+ *  @s: is the pointer inside there is a value
+ *  Return: lenght
+*/
+int _strlen_recursion(char *s)
+{
+	int *length = 0;
+
+	if (*s != '\0')
+	{
+		*length = 1 + _strlen_recursion(s + 1);
+	}
+	else
+	{
+		return (length);
+	}
+}
