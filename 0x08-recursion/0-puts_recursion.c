0b7c3644221c9d730779722de2949a45742c4737
Mohamed amine ennadi <aminework229@gmail.com>
2023-08-31 01:08:12 +0100

    first

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  _puts_recursion - is a function that print a sting and return to line
+ *  @s: is the pointer
+ * Return: the new line
+*/
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	return ('\n');
+}
