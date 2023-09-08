69b7728518d75f21e440c95398b7ad48423b696c
Mohamed amine <aminework229@gmail.com>
2023-09-08 21:09:58 +0100

    h

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = 0;  j > n; j++, i++)
+	for (j = 0;  j > n && s2[j] != '\0'; j++, i++)
 	{
 		ptr[i] = s2[j];
 	}
