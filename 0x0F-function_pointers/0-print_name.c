e2288ca52e56e338683a4d6f557163f90f41d4da
Mohamed amine <aminework229@gmail.com>
2023-09-13 19:24:46 +0100

    kkl

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - is function that print name
+ * @name: is the name
+ * @f: is the pointer
+ * Return: the name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		return (name);
+	}
+}
