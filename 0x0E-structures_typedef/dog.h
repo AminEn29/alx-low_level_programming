a4e4ec21aeb9fea116af859e43dd1ca8e538de20
Mohamed amine <aminework229@gmail.com>
2023-09-13 01:56:04 +0100

    hilk

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- is structure for dogs with:
+ * @name: the name
+ * @age: the age
+ * @owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
