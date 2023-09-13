e3335b5bc89136d2762243e989cf77c3ea0917a2
Mohamed amine <aminework229@gmail.com>
2023-09-13 20:29:01 +0100

    kllkj

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * array_iterator - is function excuse another function size
@@ -10,9 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int	for_the_loop;
 
-	if (array == 0)
-		return;
-	if (action == 0)
+	if (array == 0 || action == 0)
 		return;
 	while (for_the_loop = 0 && for_the_loop < (int) size)
 	{
