996d13b1cf4ce9cd18840eb2a92dbc402c31fb58
Mohamed amine <aminework229@gmail.com>
2023-09-13 02:12:05 +0100

    kjl

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - function the print the dog information
+ * @d: is the pointer
+*/
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if (d->name == 0)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == 0)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
