30b8c67f0ee87207d2d4a17df9379f2e5aa4bd07
Mohamed amine <aminework229@gmail.com>
2023-09-15 01:54:57 +0100

    khilh

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <variadic_functions.h>
+/**
+ * sum_them_all - is function that sum integers
+ * Return: the number
+ * @n:is the parametre
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+
+	va_start(args, n);
+	int sum = 0;
+
+	for (unsigned int i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
