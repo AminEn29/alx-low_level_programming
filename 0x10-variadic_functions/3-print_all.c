fa70fc64e819300c867d2736d3f8f80414c11e9b
AminEn29 <138697936+AminEn29@users.noreply.github.com>
2023-09-21 22:34:12 +0100

    Hi

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,6 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
-
-void print_all(const char * const format, ...)
-{
-    va_list args;
-    int i = 0;
-    char *str;
-    char c;
-    float f;
-
-    va_start(args, format);
-
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                c = va_arg(args, int);
-                _putchar(c);
-                break;
-
-            case 'i':
-                i = va_arg(args, int);
-                print_number(i);
-                break;
-
-            case 'f':
-                f = va_arg(args, double); // float arguments are promoted to double in va_arg
-                print_float(f);
-                break;
-
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    print_string("(nil)");
-                else
-                    print_string(str);
-                break;
-
-            default:
-                break;
-        }
-
-        if ((format[i + 1]) && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-            _putchar(',');
-
-        i++;
-    }
-
-    va_end(args);
-    _putchar('\n');
-}
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * 
+*/
--- README.md
+++
@@ -1,3 +0,0 @@
-# alx-low_level_programming
-# For programming in C 
-By Mohamed amine ennadi
