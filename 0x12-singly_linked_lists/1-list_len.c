f08f001afd0a0e3b4b63dd800cf9f885f488a272
AminEn29 <138697936+AminEn29@users.noreply.github.com>
2023-09-21 23:13:09 +0100

    hi

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
  * list_len - is function that print number of elements
  * @h: is the pointer
@@ -9,13 +8,13 @@ size_t list_len(const list_t *h)
 {
 	int counte_links = 0;
 
-	if (h == NULL)
+	if (h == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		while (h != NULL)
+		while (h != 0)
 		{
 			counte_links++;
 			h = h->next;
