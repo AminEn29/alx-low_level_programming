03f800592d5977551b2aef2ed485f246e12045ef
Mohamed amine <aminework229@gmail.com>
2023-10-05 04:08:53 +0100

    hi

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# a script that 
-# callles
-# that are in y
-
-gcc -c *.c
-ar rc liball.a *.o
-echo y | rm -I *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
