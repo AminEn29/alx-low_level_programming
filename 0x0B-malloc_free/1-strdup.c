21b1e61e7d9cf45a2212f3adcde9e579e48bc6d6
Mohamed amine <aminework229@gmail.com>
2023-10-05 01:40:06 +0100

    hello

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - is function that allocat sapce for string
+ * @str: is the string
+ * Return: NULL if str=NULL
+*/
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	char *space = (char *)malloc(1 * (i + 1));
+
+	for (int j = 0; j <= i; j++)
+		space[j] = str[j];
+	return (space);
+}
