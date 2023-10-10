fd39a019b6738c26796bb8773aad51a38ea411fc
AminEn29 <138697936+AminEn29@users.noreply.github.com>
2023-10-10 16:54:34 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,5 +0,0 @@
-#ifndef _OBJECT_LIKE_MACRO_h
-#define _OBJECT_LIKE_MACRO_h
-
-#define SIZE 1024
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef _FUNCION_LIKE_MACRO_H
-#define _FUNCION_LIKE_MACRO_H
-
-
-#define ABS(x) ((x) > 1 ? ((x) * 1) : ((x) * -1))
-
-#endif
